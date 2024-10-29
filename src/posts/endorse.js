8789bfd9a7774d33854e89af3677045dee97bcac
Owen Gometz <owengometz@gmail.com>
2024-10-28 22:22:40 -0400

    fixes after running command px standard --fix

--- src/posts/endorse.js
+++ src/posts/endorse.js
@@ -1,27 +1,27 @@
-'use strict';
+'use strict'
 
-const plugins = require('../plugins');
+const plugins = require('../plugins')
 
 module.exports = function (Posts) {
-	Posts.endorse = async function (pid, uid) {
-		return await toggleEndorse('endorse', pid, uid);
-	};
+  Posts.endorse = async function (pid, uid) {
+    return await toggleEndorse('endorse', pid, uid)
+  }
 
-	Posts.unendorse = async function (pid, uid) {
-		return await toggleEndorse('unendorse', pid, uid);
-	};
+  Posts.unendorse = async function (pid, uid) {
+    return await toggleEndorse('unendorse', pid, uid)
+  }
 
-	async function toggleEndorse(type, pid, uid) {
-		const isEndorsing = type === 'endorse';
-		await plugins.hooks.fire(`filter:post.${type}`, { pid: pid, uid: uid });
+  async function toggleEndorse (type, pid, uid) {
+    const isEndorsing = type === 'endorse'
+    await plugins.hooks.fire(`filter:post.${type}`, { pid, uid })
 
-		await Posts.setPostFields(pid, {
-			endorse: isEndorsing ? 1 : 0,
-			endorseUid: isEndorsing ? uid : 0,
-		});
+    await Posts.setPostFields(pid, {
+      endorse: isEndorsing ? 1 : 0,
+      endorseUid: isEndorsing ? uid : 0
+    })
 
-		const postData = await Posts.getPostFields(pid, ['pid', 'tid', 'uid', 'content', 'timestamp']);
+    const postData = await Posts.getPostFields(pid, ['pid', 'tid', 'uid', 'content', 'timestamp'])
 
-		return postData;
-	}
-};
+    return postData
+  }
+}
